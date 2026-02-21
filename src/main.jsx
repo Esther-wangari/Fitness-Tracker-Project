059d61af5de2523e056002fbb4dc18ac01a0e2e0
Esther <estherwaiganjo00@gmail.com>
2026-02-21 10:29:21 +0300

    Fix React 18 root setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,11 @@ import React from 'react';
 import { ReactDOM } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import ErrorBoundary from './components/ErrorBoundary';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 <React.StrictMode>
-  <BrowserRouter>
-    <ErrorBoundary>
       <App />
-    </ErrorBoundary>
-  </BrowserRouter>
 </React.StrictMode>
 );
 
