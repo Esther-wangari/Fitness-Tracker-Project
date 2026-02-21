1fa7090573e4472fb62ff5bc533d37e12c6d068b
Esther <estherwaiganjo00@gmail.com>
2026-02-21 10:15:20 +0300

    Fix ReactDOM import

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import { ReactDOM } from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
+import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import ErrorBoundary from './components/ErrorBoundary';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
+const root = createRoot(document.getElementById('root'));
+<React.StrictMode>
   <BrowserRouter>
     <ErrorBoundary>
       <App />
     </ErrorBoundary>
-  </BrowserRouter>,
-);
+  </BrowserRouter>
+</React.StrictMode>
+
+
+
